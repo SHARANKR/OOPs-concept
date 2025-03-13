5c03effe3d32e57f43212d95f43477918fa8c996
SHARANKR <sharankr798@gmail.com>
2025-03-13 12:45:47 +0530

    2. Chatbook menu outlook stage complete

--- oops_proj.py
+++ oops_proj.py
@@ -4,7 +4,28 @@ class chatbook:
         self.username = ""
         self.password = ""
         self.loggedin = False
-       
+        self.menu()
+        option = input("Enter the number to choose the operation-> ")
+        
+    def menu(self):
+        print(""" This is the menu to choose from:
+              Pick a number for the desired operation
+              1. To signup
+              2. To signin
+              3. To post something
+              4. To send a message to a friend
+              5. To end the menu""")
+        
+        if option == 1:
+            pass
+        elif option == 2:
+            pass
+        elif option == 3:
+            pass
+        elif option == 4:
+            pass
+        else:
+            exit()
         
 chat = chatbook()
 
